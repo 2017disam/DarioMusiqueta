0ce29ad6aefeb1fc4e92fe88077c4fe0960fce22
2017disam <2017disam@gmail.com>
2026-01-15 23:33:47 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,3 @@
-const CACHE_NAME = 'mtv-player-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/mtv-logo.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', function(event) {
+    // Necesario para que Android permita la instalación
 });
